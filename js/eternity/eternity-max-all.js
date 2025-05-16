734e3237dae2c8a76e7a84c789127c52b9f85527
Darkwing3125 <thedarklord2317@gmail.com>
2025-05-16 12:54:23 +0800

    Update eternity-max-all.js

--- js/eternity/eternity-max-all.js
+++ js/eternity/eternity-max-all.js
@@ -10,23 +10,23 @@ let EternityMaxAll = {
   },
   maxAll(types) {
     if (types === undefined) {
-      types = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+      types = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     }
     generalHighestSweep(() => EternityGenerators.highest(), types);
-    generalMaxAll(types.filter(x => x > 8 && x <= 11).map(x => EternityUpgrade(x - 8)), EternityPoints);
-    generalMaxAll(types.filter(x => x <= 8).map(x => EternityGenerator(x)), EternityPoints);
-    generalMaxAll(types.filter(x => x > 11 & x <= 13).map(x => EternityProducerUpgrade(x - 11)), EternityPoints);
+    generalMaxAll(types.filter(x => x > 9 && x <= 12).map(x => EternityUpgrade(x - 8)), EternityPoints);
+    generalMaxAll(types.filter(x => x <= 9).map(x => EternityGenerator(x)), EternityPoints);
+    generalMaxAll(types.filter(x => x > 12 & x <= 14).map(x => EternityProducerUpgrade(x - 11)), EternityPoints);
     for (let i of [0, 1, 2]) {
-      if (types.includes(i + 14)) {
+      if (types.includes(i + 15)) {
         Studies.buyMax(i);
       }
     }
     generalMaxAll(types.filter(x => x > 16).map(x => PermanenceUpgrade(x - 16)), Permanence);
   },
   maxUpgrades() {
-    this.maxAll([9, 10, 11]);
+    this.maxAll([10, 11, 12]);
   },
   maxGenerators() {
-    this.maxAll([1, 2, 3, 4, 5, 6, 7, 8]);
+    this.maxAll([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   }
 }
